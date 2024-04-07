20b43c4e8c9e7be796fe1325b912dc62de945da0
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-07 04:09:02 -0400

    optimize training loop to not include evaluation interspersed

--- utils/steps.py
+++ utils/steps.py
@@ -27,18 +27,9 @@ def train_net(model, train_dataloader, optimizer, scheduler):
         optimizer.step()
         scheduler.step()
 
-        with torch.no_grad():
-            wer, ser = compute_error_rate(output, text)
-            epoch_loss += loss.item()
-            epoch_wer += wer
-            epoch_ser += ser
-
-    return {
-        'loss': epoch_loss / len(train_dataloader),
-        'wer': epoch_wer / len(train_dataloader),
-        'ser': epoch_ser / len(train_dataloader)
-    }
-
+    # evaluating the model 
+    train_metrics = valid_net(model, train_dataloader)
+    return train_metrics
 
 def valid_net(model, valid_dataloader):
     model.eval()
