28ce8ac798251d70866a76649e7ff10c11b47da6
Nguyen Manh Tan <tan@tanmaclocalhost.local>
2024-04-11 18:31:25 -0400

    fixed bug file to file

--- files/spk2spk.py
+++ files/spk2spk.py
@@ -1,13 +1,15 @@
-num_spks = 1e9 # all 
-
-spks = []
+# num_spks = 1e9 # all 
+num_spks = 500
+spks = set()
 
 with open("egs/tedlium/data/train/spk2utt", "r") as file:
     for line in file:
         content = line.split()
         spk_id = content[0].split("-")[0].split("_")[0]
-        spks.append(spk_id)
+        spks.add(spk_id)
         if len(spks) == num_spks:
             break
 
 assert len(spks) <= num_spks
+
+spks = list(spks)
