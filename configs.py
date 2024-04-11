24ca82b432750bb887bb68e155a9433311a6ac86
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-10 22:54:02 -0400

    adjust batchsize back to 16

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ from whisper.normalizers import EnglishTextNormalizer
 # speech recognition training configurations 
 decode_option = whisper.DecodingOptions(language="en", without_timestamps=True)
 speech_recognition_cfg = {
-    'batch_size': 40,
+    'batch_size': 16,
     'epochs': 30,
     'learning_rate': 1e-5,
     'no_decay': ["bias", "LayerNorm.weight"],
