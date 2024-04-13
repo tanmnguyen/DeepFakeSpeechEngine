04fa61305603bac56bfb12d62fe585fc5ce5e81b
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-12 22:49:19 -0400

    add fc on top of encoder

--- models/MelGenerator.py
+++ models/MelGenerator.py
@@ -5,21 +5,19 @@ class MelGenerator(nn.Module):
     def __init__(self, input_channels: int):
         super(MelGenerator, self).__init__()
         
-        # self.encoder = nn.TransformerEncoder(
-        #     nn.TransformerEncoderLayer(d_model=input_channels, nhead=8, batch_first=True), # encoder layer 
-        #     num_layers=2,
-        # )
-        self.encoder = nn.Linear(input_channels, input_channels)
+        self.encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model=input_channels, nhead=8, batch_first=True), # encoder layer 
+            num_layers=2,
+        )
+        self.fc = nn.Linear(input_channels, input_channels)
 
-        # init weight so that the encoder output is the same as the input
-        nn.init.eye_(self.encoder.weight)
-        self.encoder.bias.data.zero_()
 
     def forward(self, x):
         # x shape: (batch_size, input_channels, seq_len)
 
         x = rearrange(x, 'b c t -> b t c') # rearrange to (batch_size, seq_len, input_channels)
         x = self.encoder(x)
+        x = self.fc(x)
         x = rearrange(x, 'b t c -> b c t') # rearrange to (batch_size, input_channels, seq_len)
 
         return x
