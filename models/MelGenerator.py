68cd1d88e3026f3f8f447d4d2cddd286a3363f1e
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-13 03:26:31 -0400

    improve network

--- models/MelGenerator.py
+++ models/MelGenerator.py
@@ -1,3 +1,4 @@
+import torch 
 import torch.nn as nn 
 from einops import rearrange
 
@@ -6,21 +7,30 @@ class MelGenerator(nn.Module):
         super(MelGenerator, self).__init__()
 
         self.mhsa1 = nn.MultiheadAttention(embed_dim=input_channels, num_heads=1)
-        self.mhsa2 = nn.MultiheadAttention(embed_dim=input_channels, num_heads=1)
-        self.fc = nn.Linear(input_channels, input_channels)
+        self.fc1 = nn.Linear(input_channels, input_channels)
+        self.relu1 = nn.ReLU()
+
+        self.fc2 = nn.Linear(input_channels, input_channels)
+        self.relu2 = nn.ReLU()
+
+        self.fc3 = nn.Linear(input_channels, input_channels)
+        self.relu3 = nn.ReLU()
+
 
         # init weight such that this network is an identity function
         # init weight of mhsa1 and mhsa2 to 0
         self.mhsa1.in_proj_weight.data.zero_()
         self.mhsa1.in_proj_bias.data.zero_()
-        self.mhsa2.in_proj_weight.data.zero_()
-        self.mhsa2.in_proj_bias.data.zero_()
+       
+        # make weight of fc to identity matrix using eye 
+        self.fc1.weight.data = torch.eye(input_channels)
+        self.fc1.bias.data.zero_()
 
-        # make weight of fc to identity matrix
-        self.fc.weight.data.zero_()
-        self.fc.bias.data.zero_()
-        for i in range(input_channels):
-            self.fc.weight.data[i, i] = 1.0
+        self.fc2.weight.data = torch.eye(input_channels)
+        self.fc2.bias.data.zero_()
+
+        self.fc3.weight.data = torch.eye(input_channels)
+        self.fc3.bias.data.zero_()
 
 
     def forward(self, x):
@@ -31,10 +41,13 @@ class MelGenerator(nn.Module):
         x_mhsa, _ = self.mhsa1(x, x, x)
         x = x + x_mhsa
 
-        x_mhsa, _ = self.mhsa2(x, x, x)
-        x = x + x_mhsa
+        x = self.fc1(x)
+        x = self.relu1(x)
 
-        x = self.fc(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+
+        x = self.fc3(x)
 
         x = rearrange(x, 'b t c -> b c t') # rearrange to (batch_size, input_channels, seq_len)
 
