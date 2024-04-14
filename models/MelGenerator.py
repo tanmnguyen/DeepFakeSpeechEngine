2a9941138697b6db4509c4caf41007f70cf35243
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-14 18:32:18 -0400

    disable nograd option

--- models/MelGenerator.py
+++ models/MelGenerator.py
@@ -91,7 +91,7 @@ class MelGenerator(nn.Module):
 
         loss_spk, spk_output = self.spk_model.neg_cross_entropy_loss(gen_melspec, speaker_labels)
         # loss_asr, asr_output = self.asr_model.loss_encoder(tru_melspec, gen_melspec, tokens)
-        loss_asr, asr_output = self.asr_model.loss(gen_melspec, tokens, labels)
+        loss_asr, asr_output = self.asr_model.loss(gen_melspec, tokens, labels, encoder_no_grad=False)
 
         return loss_asr, spk_output, asr_output
         # return loss_asr + loss_spk, spk_output, asr_output
