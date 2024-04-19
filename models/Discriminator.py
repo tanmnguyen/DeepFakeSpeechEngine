5040748e3e2f5cc324641dd0d6ad7d414497b450
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-19 00:27:35 -0400

    modify discriminator

--- models/Discriminator.py
+++ models/Discriminator.py
@@ -6,25 +6,27 @@ import torch.nn as nn
 from einops import rearrange
 
 class Discriminator(nn.Module):
-    def __init__(self, feat_dim=80):
+    def __init__(self, feat_dim=80, hidden_dim=128):
         super(Discriminator, self).__init__()
-        self.encoder = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(d_model=feat_dim, nhead=2, batch_first=True), 
-            num_layers=2
-        )
-
-        self.fc = nn.Linear(feat_dim, 1)
+        self.fc1 = nn.Linear(feat_dim, hidden_dim)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(hidden_dim, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, mel):
         x = rearrange(mel, 'b c t -> b t c') 
         
         # compute embeddings
-        x = self.encoder(x)
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = x.mean(dim=1)
 
         # compute the average along the feature dimension
-        x = x.mean(dim=1)
-        x = self.fc(x)
+        x = self.fc3(x)
         x = self.sigmoid(x)
 
         return x
