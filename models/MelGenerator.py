132d5d222c0ca5cb618c1d6ef5e513cb7c123e83
Nguyen Manh Tan <tan@tanmaclocalhost.local>
2024-04-19 16:20:18 -0400

    revert back to encoder based network

--- models/MelGenerator.py
+++ models/MelGenerator.py
@@ -3,59 +3,73 @@ sys.path.append("../")
 
 import torch 
 import configs 
-import torchaudio 
 import torch.nn as nn 
 import torch.optim as optim
-import torch.nn.functional as F
 
 from einops import rearrange
-from torchmetrics import Accuracy
 from .Discriminator import Discriminator
 from utils.batch import process_mel_spectrogram
 
 class Generator(nn.Module):
-    def __init__(self, in_channels=80, out_channels=80, max_length=3000):
+    def __init__(self, in_channels=80):
         super(Generator, self).__init__()
 
-        # Transformer Encoder
-        encoder_layer = nn.TransformerEncoderLayer(d_model=in_channels, nhead=2)
-        self.melspec_encoder = nn.TransformerEncoder(encoder_layer, num_layers=2)
+        self.fc = nn.Linear(in_channels, in_channels)
+        self.relu = nn.ReLU() 
 
-        # Transformer Decoder
-        decoder_layer = nn.TransformerDecoderLayer(d_model=out_channels, nhead=2)
-        self.melspec_decoder = nn.TransformerDecoder(decoder_layer, num_layers=2)
+        self.melspec_encoder_1 = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model=in_channels, nhead=2), 
+            num_layers=2
+        )
 
-        self.linear = nn.Linear(in_channels, out_channels)
+        self.melspec_encoder_2 = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model=in_channels, nhead=2), 
+            num_layers=2
+        )
+
+        self.melspec_encoder_3 = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model=in_channels, nhead=2), 
+            num_layers=2
+        )
+
+        self.fc_out = nn.Linear(in_channels, in_channels)
+
 
     def forward(self, x):
+        """ 
+        x shape: (batch_size, input_channels, seq_len) 
         """
-        x shape: (batch_size, input_channels, seq_len)
-        """
-        x = rearrange(x, 'b c t -> b t c')
+        x0 = x 
 
-        # Transformer Encoder
-        x_encoded = self.melspec_encoder(x)
+        # rearrange to (batch_size, seq_len, input_channels)
+        x = rearrange(x, 'b c t -> b t c') 
 
-        # Transformer Decoder
-        x_decoded = self.melspec_decoder(tgt=x, memory=x_encoded)
+        y = self.fc(x)
+        x = self.relu(y) + x 
 
-        # Linear transformation to output channels
-        output = self.linear(x_decoded)
+        y = self.melspec_encoder_1(x) # (batch_size, seq_len, in_channels)
+        x = self.relu(y) + x
 
-        output = rearrange(output, 'b t c -> b c t')
+        y = self.melspec_encoder_2(x) # (batch_size, seq_len, in_channels)
+        x = self.relu(y) + x
 
-        return output
-        
+        y = self.melspec_encoder_3(x) # (batch_size, seq_len, in_channels)
+        x = self.relu(y) + x
+
+        x = rearrange(x, 'b t c -> b c t') # rearrange to (batch_size, input_channels, seq_len)
+        x = self.relu(x) + x0 
+
+        return x
+
 
 class MelGenerator(nn.Module):
     def __init__(self, asr_model: nn.Module, spk_model: nn.Module):
         super(MelGenerator, self).__init__()
+        self.generator = Generator()
         self.asr_model = asr_model 
         self.spk_model = spk_model
-        self.generator = Generator()
         self.discriminator = Discriminator()
         self.dis_optimizer = optim.Adam(self.discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-        self.disc_accuracy_fn = Accuracy(task="binary")
 
     def set_gen_optimizer(self, gen_optimizer, gen_scheduler):
         self.gen_optimizer = gen_optimizer
@@ -84,25 +98,27 @@ class MelGenerator(nn.Module):
         # train adversarial discriminator
         self.discriminator.zero_grad()
         self.dis_optimizer.zero_grad()
-        loss_d_real, d_out_real = self.discriminator.loss(x, torch.ones(x.shape[0],).to(configs.device))
-        loss_d_fake, d_out_fake = self.discriminator.loss(gen_melspec.detach(), torch.zeros(x.shape[0],).to(configs.device))
-        d_acc = (self.disc_accuracy_fn(d_out_fake, torch.zeros(x.shape[0],).to(configs.device)) + \
-                self.disc_accuracy_fn(d_out_real, torch.ones(x.shape[0],).to(configs.device))) / 2
+        loss_d_real = self.discriminator.loss(x, torch.ones(x.shape[0], 1).to(configs.device))
+        loss_d_fake = self.discriminator.loss(gen_melspec.detach(), torch.zeros(x.shape[0], 1).to(configs.device))
         loss_d = loss_d_real + loss_d_fake
         loss_d.backward()
         self.dis_optimizer.step()
         
         # train content 
         processed_gen_melspec = process_mel_spectrogram(gen_melspec)
-        processed_tru_melspec = process_mel_spectrogram(x)
         loss_spk, spk_output = self.spk_model.loss(processed_gen_melspec, speaker_labels)
-        # loss_asr, asr_output = self.asr_model.loss(processed_gen_melspec, tokens, labels, encoder_no_grad=False)
-        loss_asr, asr_output = self.asr_model.loss_encoder(processed_tru_melspec, processed_gen_melspec, tokens)
-        adv_gen_loss, _ = self.discriminator.loss(gen_melspec, torch.ones(x.shape[0],).to(configs.device))
-        loss = loss_asr / loss_spk + adv_gen_loss
+        loss_asr, asr_output = self.asr_model.loss(processed_gen_melspec, tokens, labels, encoder_no_grad=False)
+        loss = loss_asr / loss_spk 
+        loss.backward()
+
+        # train adversarial generator 
+        self.gen_optimizer.zero_grad()
+        self.zero_grad() 
+        labels = torch.ones(x.shape[0], 1).to(configs.device)
+        adv_gen_loss = self.discriminator.loss(gen_melspec, labels)
+        adv_gen_loss.backward()
 
         # update generator
-        loss.backward()
         self.gen_optimizer.step()
         
         if self.gen_optimizer.param_groups[0]['lr'] >= configs.mel_generator_cfg['min_lr']:
@@ -114,7 +130,7 @@ class MelGenerator(nn.Module):
                 x.contiguous().view(x.shape[0], -1)
             )
 
-        return loss, spk_output, asr_output, mel_mse, loss_spk, loss_asr, d_acc
+        return loss, spk_output, asr_output, mel_mse, loss_spk, loss_asr, loss_d
     
     def train_speaker_recognizer(self, x, speaker_labels):
         self.spk_model.train() 
