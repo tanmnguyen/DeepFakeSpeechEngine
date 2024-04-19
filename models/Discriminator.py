4a17b2a28ffa76ebb3ce64b565384d848e157872
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-18 23:45:46 -0400

    change discriminator architecture

--- models/Discriminator.py
+++ models/Discriminator.py
@@ -4,28 +4,26 @@ sys.path.append('../')
 import torch.nn as nn
 
 from einops import rearrange
-from utils.metrics import neg_cross_entropy_loss
-from .ECAPA_TDNN.ecapa_tdnn import ECAPA_TDNN_GLOB_c512 
 
 class Discriminator(nn.Module):
-    def __init__(self, feat_dim=80, embed_dim=256, pooling_func='ASTP'):
+    def __init__(self, feat_dim=80):
         super(Discriminator, self).__init__()
-        self.encoder = ECAPA_TDNN_GLOB_c512(
-            feat_dim=feat_dim,
-            embed_dim=embed_dim,
-            pooling_func=pooling_func
+        self.encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model=feat_dim, nhead=2), 
+            num_layers=2
         )
-        self.dropout = nn.Dropout(0.3)
-        self.fc = nn.Linear(embed_dim, 1)
+
+        self.fc = nn.Linear(feat_dim, 1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, mel):
-        x = rearrange(mel, 'b t c -> b c t')
+        x = rearrange(mel, 'b c t -> b t c') 
+        
         # compute embeddings
         x = self.encoder(x)
-        # apply dropout
-        x = self.dropout(x)
-        # classification
+
+        # compute the average along the feature dimension
+        x = x.mean(dim=1)
         x = self.fc(x)
         x = self.sigmoid(x)
 
