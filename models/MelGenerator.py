1c1d8ddeecb8f3c24da15f342b455f3ce595ba1d
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-04-24 22:08:12 -0400

    update generator to include shift up and shift down operations

--- models/MelGenerator.py
+++ models/MelGenerator.py
@@ -11,9 +11,9 @@ from einops import rearrange
 from .Discriminator import Discriminator
 from utils.batch import process_mel_spectrogram
 
-class Generator(nn.Module):
+class MelShiftNetwork(nn.Module):
     def __init__(self, in_channels=80):
-        super(Generator, self).__init__()
+        super(MelShiftNetwork, self).__init__()
 
         self.fc = nn.Linear(in_channels, in_channels)
         self.relu = nn.ReLU() 
@@ -35,7 +35,6 @@ class Generator(nn.Module):
             num_layers=2
         )
         self.fc3 = nn.Linear(in_channels, in_channels)
-
         self.fc_out = nn.Linear(in_channels, in_channels)
 
     def forward(self, x):
@@ -44,7 +43,6 @@ class Generator(nn.Module):
         """
         # rearrange to (batch_size, seq_len, input_channels)
         x = rearrange(x, 'b c t -> b t c') 
-        x0 = x
 
         y = self.fc(x)
         x = self.relu(y) + x 
@@ -62,7 +60,29 @@ class Generator(nn.Module):
         x = self.relu(y + x)
 
         x = self.fc_out(x)  
-        x = self.relu(x) + x0 
+
+        return x
+
+class Generator(nn.Module):
+    def __init__(self, in_channels=80):
+        super(Generator, self).__init__()
+
+        self.mel_shift_net_up = MelShiftNetwork(in_channels=in_channels)
+        self.mel_shift_net_dn = MelShiftNetwork(in_channels=in_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        """ 
+        x shape: (batch_size, input_channels, seq_len) 
+        """
+        # rearrange to (batch_size, seq_len, input_channels)
+        x = rearrange(x, 'b c t -> b t c') 
+        x0 = x
+
+        shift_up = self.mel_shift_net_up(x)
+        shift_dn = self.mel_shift_net_dn(x)
+
+        x = self.relu(shift_up) + x0 - self.relu(shift_dn)
 
         x = rearrange(x, 'b t c -> b c t') # rearrange to (batch_size, input_channels, seq_len)
         return x
